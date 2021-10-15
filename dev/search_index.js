47d772ca6920de5af21587a24f74da35b348c4a4
Documenter.jl <documenter@juliadocs.github.io>
2021-10-15 10:27:28 +0000

    build based on 93ba14a

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = PrettyMacroExpand","category":"page"},{"location":"#PrettyMacroExpand","page":"Home","title":"PrettyMacroExpand","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for PrettyMacroExpand.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [PrettyMacroExpand]","category":"page"}]
+}
